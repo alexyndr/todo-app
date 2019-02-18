a3e7910587e945abf4a15766b3f8fa984439fe4e
alex <alexyndr@gmail/com>
2019-02-19 01:46:46 +0200

    Sample of main list

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,7 +1,6 @@
 class TodoItemsController < ApplicationController
-
-	before_action :set_todo_list, except: :complete
-	before_action :set_todo_item, except: [:create, :new, :complete]
+	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create, :new]
 
 	def new
 		@todo_item = @todo_list.todo_items.new
@@ -9,7 +8,7 @@ class TodoItemsController < ApplicationController
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
-		redirect_to user_todo_list_path(current_user, @todo_list)
+		redirect_to user_todo_lists_path(current_user)
 	end
 
 	def destroy
@@ -18,7 +17,7 @@ class TodoItemsController < ApplicationController
 		else
 			flash[:error] = "Todo List item could not be deleted"
 		end
-		redirect_to user_todo_list_path(current_user, @todo_list)
+		redirect_to user_todo_lists_path(current_user)
 	end
 
 	def edit
@@ -26,15 +25,15 @@ class TodoItemsController < ApplicationController
 
   def update
     if @todo_item.update(todo_item_params)
-    	redirect_to user_todo_list_path(current_user, @todo_list)
+    	redirect_to user_todo_lists_path(current_user)
     end
   end
 
 	def complete
-		@todo_list = TodoList.find(params[:id])
-		@todo_item = @todo_list.todo_items.find(params[:todo_list_id])
+		@todo_list = TodoList.find(params[:todo_list_id])
+		@todo_item = @todo_list.todo_items.find(params[:id])
 		@todo_item.update_attribute(:completed_at, Time.now)
-		redirect_to  user_todo_list_path(current_user, @todo_list), notice: "Todo item completed"
+		redirect_to user_todo_lists_path(current_user), notice: "Todo item completed"
 	end
 
 	private
--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,5 @@
 class TodoListsController < ApplicationController
-  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_todo_list, only: [ :edit, :update, :destroy]
   before_action :authenticate_user!
   before_action :check_user
 
